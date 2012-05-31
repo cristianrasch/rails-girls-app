96410795f72bbd17a74b532b983822a77efeb5c0
Cristian Rasch <cristianrasch@gmail.com>
2012-05-31 10:40:43 -0300

    Added a challenge in the form of a pending validation test

--- test/unit/idea_test.rb
+++ test/unit/idea_test.rb
@@ -1,6 +1,8 @@
 require 'test_helper'
 
 class IdeaTest < ActiveSupport::TestCase
+  include ActiveSupport::Testing::Pending
+
   test "name can't be blank" do
     idea = Idea.new
     assert idea.invalid?
@@ -15,5 +17,13 @@ class IdeaTest < ActiveSupport::TestCase
     assert new_idea.invalid?
     assert new_idea.errors[:name].present?
     assert new_idea.errors[:name].include?('has already been taken')
+  end
+  
+  test "either its description or picture must be present" do
+    pending "well, we'll see about that.."
+    idea = FactoryGirl.create(:idea, description: ' ', picture: nil)
+    assert idea.invalid?
+    assert new_idea.errors.get(:base).present?
+    assert new_idea.errors.get(:base).any? { |err| err =~ /either description or picture must be filled in/i }
   end
 end
